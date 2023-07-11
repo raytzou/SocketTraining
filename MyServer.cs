3142b8c3b6ed9207ebf2af76af73e24f207e7832
raytzou <raytzou@outlook.com>
2023-07-12 03:56:39 +0800

    change to use TCPListener

---
+++ MyServer.cs
@@ -0,0 +1,120 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MyServer
+{
+    static void Main()
+    {
+        MyTCPListener training = new();
+
+        training.RunServer();
+    }
+}
+
+class MyTCPListener
+{
+    private readonly IPEndPoint _endPoint;
+    private static List<TcpClient> _clientList = null!;
+
+    public MyTCPListener()
+    {
+        _endPoint = new(IPAddress.Any, 8787); // broadcast, any = IP: 0.0.0.0
+        _clientList = new List<TcpClient>();
+    }
+
+    enum MessageCategory
+    {
+        Ack,Msg
+    }
+
+    public void RunServer()
+    {
+        TcpListener listener = new(_endPoint);
+
+        listener.Start();
+
+        Console.WriteLine($"Server is hosting, port: {_endPoint.Port}");
+        
+        Thread fork = new(ClientHandle);
+        fork.Start();
+        
+        while (true)
+        {
+            var client = listener.AcceptTcpClient();
+
+            lock (_clientList) // Runtime Exception: Collection was modified; enumeration operation may not execute.
+            {
+                _clientList.Add(client);
+            }
+            Console.WriteLine($"{DateTime.Now} A client has connected.");
+        }
+    }
+
+    private void ClientHandle() // async for keeping handling client
+    {
+        while (true)
+        {
+            lock ( _clientList) // Runtime Exception: Collection was modified; enumeration operation may not execute.
+            {
+                foreach (var client in _clientList)
+                {
+                    if (client.Available > 0) // when we got some datas (we all know that is just message) from the god damn client
+                    {
+                        ReadMessage(client);
+                        ServerAcknowledge(client);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void ReadMessage(TcpClient client)
+    {
+        try
+        {
+            NetworkStream stream = client.GetStream();
+            var buffer = new byte[512];
+            var receiver = stream.Read(buffer);
+            var decoder = Encoding.UTF8.GetString(buffer);
+
+            Console.WriteLine($"{DateTime.Now} {decoder}");
+            SendMessage(decoder, client);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{DateTime.Now} {ex.Message}");
+        }
+    }
+
+    /// <summary>
+    /// Send message to everyone, exclude itself.
+    /// </summary>
+    /// <param name="message">string: message</param>
+    /// <param name="theClient">TcpClient: The message sender</param>
+    private static void SendMessage(string message, TcpClient theClient)
+    {
+        foreach(var client in _clientList)
+        {
+            if (client == theClient) continue;
+
+            NetworkStream stream = client.GetStream();
+            var msgBytes = Encoding.UTF8.GetBytes(MessageCategory.Msg + "$" + message);
+            
+            stream.Write(msgBytes, 0, msgBytes.Length);
+        }
+    }
+
+    /// <summary>
+    /// Server acknowledge. For client that uses Socket.Receive() which is blocking method.
+    /// </summary>
+    /// <param name="client"></param>
+    private static void ServerAcknowledge(TcpClient client)
+    {
+        NetworkStream stream = client.GetStream();
+        var echo = $"{MessageCategory.Ack}${DateTime.Now}$Server acknowledge.";
+        var encode = Encoding.UTF8.GetBytes(echo);
+        
+        stream.Write(encode, 0, encode.Length);
+    }
+}
--- MySocketClient/MyClient.cs
+++ MySocketClient/MyClient.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using System.Text;
 
-class Program
+class MyClient
 {
     static async Task Main()
     {
@@ -20,9 +20,9 @@ class MySocketClient
 
     public MySocketClient()
     {
-        _ipHostInfo = Dns.GetHostEntry(Dns.GetHostName()); // claim an entry for hosting a simple server. Dns.GetHostName() == localhost
-        _ipAddress = _ipHostInfo.AddressList[0]; // get entry's IP, it contains ipv4, ipv6, lan ip, wan ip...
-        _ipEndPoint = new(_ipAddress, 8787); // endpoint can be used for client to connect, listener for host. port number = 8787
+        _ipHostInfo = Dns.GetHostEntry("127.0.0.1");
+        _ipAddress = _ipHostInfo.AddressList[0];
+        _ipEndPoint = new(_ipAddress, 8787);
     }
 
     /// <summary>
@@ -34,20 +34,23 @@ class MySocketClient
         using Socket client = new(_ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp); // claim a Socket client, use TCP protocol
 
         Console.WriteLine("Connect to " + _ipAddress.ToString());
+        Console.WriteLine("Please Enter your name: ");
 
+        var name = Console.ReadLine();
+        if (string.IsNullOrEmpty(name)) name = "Unknown";
         /*TcpListener listener = new(_ipEndPoint); // for testing client
         listener.Start();*/ // for testing client
         await client.ConnectAsync(_ipEndPoint); // socket client connects to listener server, should RunServer() first
 
         while (true)
         {
-            Console.Write("\nClient Type: ");
-            string msg = Console.ReadLine();
+            Console.Write("\nSay: ");
+            var msg = Console.ReadLine();
 
             if (string.IsNullOrEmpty(msg)) continue;
 
             #region Sender
-            var msgBytes = Encoding.UTF8.GetBytes(msg);
+            var msgBytes = Encoding.UTF8.GetBytes(name + "$" + msg + "$" + _ipEndPoint.Address);
             var sender = await client.SendAsync(msgBytes, SocketFlags.None); // send message, return how many bytes have been sent
             // socketflags specifies socket send and receive behaviors
             // https://learn.microsoft.com/en-us/dotnet/api/system.net.sockets.socketflags?view=net-7.0
@@ -56,7 +59,7 @@ class MySocketClient
             if (sender > 0) Console.WriteLine($"message has been sent. (message: {msg})");
 
             #region Receiver
-            var buffer = new byte[32]; // cuz we all know message has been encoded in bytes, store msg from receiver
+            var buffer = new byte[128]; // cuz we all know message has been encoded in bytes, store msg from receiver
             var receiver = await client.ReceiveAsync(buffer, SocketFlags.None); // get an ecoding message from sender
             /*should not use Receive(), it's a blocking call, if there is no available data, Receive() will block until data is available*/
             var decoder = Encoding.UTF8.GetString(buffer, 0, receiver); // decode buffer, index 0, how many bytes we have to decode
